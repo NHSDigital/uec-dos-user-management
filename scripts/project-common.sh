e27cb50697b9707499924e3952dcade3b3100473
Tim Rickwood <tim.rickwood@nhs.net>
2023-08-23 16:12:34 +0100

    DR-357 Refactor repo name

--- scripts/project-common.sh
+++ scripts/project-common.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # Used by bootstrapper and infra-deploy scripts so defined once here
-export REPO_NAME=uec-dos-user-management
+export TF_VAR_repo_name=uec-dos-user-management
 export ACCOUNT_PROJECT=dos
